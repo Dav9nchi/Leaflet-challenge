8c13c68e173f7cbd2a0c821cee8b09449dc0325b
Dav9nchi <David.castanoe@yahoo.com>
2024-02-18 13:55:23 -0500

    initial documents

---
+++ Leaflet-Part-1/static/js/logic.js
@@ -0,0 +1,10 @@
+// USGS URl All Earthquakes from the Past 7 Days
+let url : "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Using Leaflet, create a map that plots all the earthquakes from your dataset based on their longitude and latitude.
+// 1. Your data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color.
+// 2. Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in color.
+//  Hint: The depth of the earth can be found as the third coordinate for each earthquake.
+// 3. Include popups that provide additional information about the earthquake when its associated marker is clicked.
+// 4. Create a legend that will provide context for your map data.
+// 5. Your visualization should look something like the preceding map.
